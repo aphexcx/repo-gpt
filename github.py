b194715d59264cd1eb23bc88b7da390d28cac2a9
afik.cohen <afik.cohen@bytedance.com>
2023-05-08 20:54:07 -0700

    Skip loading binary files

--- github.py
+++ github.py
@@ -6,25 +6,35 @@ from langchain.vectorstores import DeepLake
 from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 import openai
+import logging
 
 load_dotenv()
 openai.api_key = os.environ.get('OPENAI_API_KEY')
+logging.basicConfig(level=logging.INFO)
 
 
 def clone_repository(repo_url, local_path):
     subprocess.run(["git", "clone", repo_url, local_path])
 
 
+def is_binary(file_path):
+    """Check if the file is binary."""
+    with open(file_path, 'rb') as file:
+        chunk = file.read(1024)
+    return b'\0' in chunk
+
+
 def load_docs(root_dir):
     docs = []
     for dirpath, dirnames, filenames in os.walk(root_dir):
         for file in filenames:
-            try:
-                loader = TextLoader(os.path.join(
-                    dirpath, file), encoding='utf-8')
-                docs.extend(loader.load_and_split())
-            except Exception as e:
-                pass
+            file_path = os.path.join(dirpath, file)
+            if not is_binary(file_path):
+                try:
+                    loader = TextLoader(file_path, encoding='utf-8')
+                    docs.extend(loader.load_and_split())
+                except Exception as e:
+                    logging.error(f"Error loading file {file}: {str(e)}")
     return docs
 
 
@@ -34,13 +44,10 @@ def split_docs(docs):
 
 
 def main(repo_url, root_dir, deep_lake_path):
-#     clone_repository(repo_url, root_dir)
     docs = load_docs(root_dir)
     texts = split_docs(docs)
     embeddings = OpenAIEmbeddings()
-    
     db = DeepLake(dataset_path=deep_lake_path, embedding_function=embeddings)
-    
     db.add_documents(texts)
 
 
@@ -48,5 +55,4 @@ if __name__ == "__main__":
     repo_url = os.environ.get('REPO_URL')
     root_dir = "/Users/afik_cohen/repos/ppl-ai/clean-android/"
     deep_lake_path = os.environ.get('DEEPLAKE_DATASET_PATH')
-
     main(repo_url, root_dir, deep_lake_path)
